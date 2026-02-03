25e7e1eb0f81723f90b0614221cabb17510813c6
mykuromae <129787024+mykuromae@users.noreply.github.com>
2026-02-03 14:21:44 -0800

    updated for mongo

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,20 +1,78 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+  }, [] );
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
     });
-    setCharacters(updated);
+
+    return promise;
+  }
+
+  function deleteUser(id) {
+    const promise = fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE"
+    });
+
+    return promise;
+  }
+
+  function removeOneCharacter(id) {
+    deleteUser(id)
+    .then((res) => {
+      if (res.status === 204) {
+        const updated = characters.filter((character) => character._id != id);
+        setCharacters(updated);
+      }
+
+      else if (res.status === 404) {
+        console.log('User not found on server');
+      }
+
+      else {
+        throw new Error('Delete failed');
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })   
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201) { // check for 201
+          return res.json(); // call response.json
+        }
+        else {
+          throw new Error('Failed to add user');
+        }
+      })
+      .then((newUser) => setCharacters([...characters, newUser]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   return (
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -5,6 +5,7 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>ID</th>
         <th>Name</th>
         <th>Job</th>
         <th>Remove</th>
@@ -16,11 +17,12 @@ function TableHeader() {
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
-      <tr key={index}>
+      <tr key={row._id}>
+        <td>{row._id}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>
-            <button onClick={() => props.removeCharacter(index)}>
+            <button onClick={() => props.removeCharacter(row._id)}>
                 Delete
             </button>
         </td>
